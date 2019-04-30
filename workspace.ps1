7d5efe0a63612e6f9b6e8ff246e60c07b8d9ac1a
Kåre Schmidt <kare.schmidt@also.com>
2019-04-30 08:12:27 +0200

    dunno

---
+++ workspace.ps1
@@ -0,0 +1,3 @@
+az resource show --ids /subscriptions/c109b571-42a3-4f92-82f1-1a1638511a7e/resourceGroups/kaaresting/providers/Microsoft.OperationalInsights/workspaces/ALSOLogAnalytics247/providers/Microsoft.SecurityInsights/settings/Fusion --api-version 2019-01-01-preview --set properties.IsEnabled=true --subscription "c109b571-42a3-4f92-82f1-1a1638511a7e"
+
+az resource show --ids /subscriptions/c109b571-42a3-4f92-82f1-1a1638511a7e/resourceGroups/kaaresting/providers/Microsoft.OperationalInsights/workspaces/ALSOLogAnalytics247/providers/Microsoft.SecurityInsights/settings/Fusion --api-version 2019-01-01-preview --subscription "c109b571-42a3-4f92-82f1-1a1638511a7e"
