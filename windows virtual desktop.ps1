930744fe232ef085942180958f8e2d9bace6f768
kaareschmidtalso <37620548+kaareschmidtalso@users.noreply.github.com>
2019-05-20 15:27:05 +0200

    Worked with windows virtual desktop

--- windows virtual desktop.ps1
+++ windows virtual desktop.ps1
@@ -1,11 +1,27 @@
-Install-Module -Name Microsoft.RDInfra.RDPowerShell
-Import-Module -Name Microsoft.RDInfra.RDPowerShell
+# windows virtual desktop
 
-#Sign in to Windows Virtual Desktop using the TenantCreator user account with this cmdlet:
+Import-Module AzureAD
+$aadContext = Connect-AzureAD
+$svcPrincipal = New-AzureADApplication -AvailableToOtherTenants $true -DisplayName "Windows Virtual Desktop Svc Principal"
+$svcPrincipalCreds = New-AzureADApplicationPasswordCredential -ObjectId $svcPrincipal.ObjectId
+
+
+$myTenantName = "alsoacademy"
+
 Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
+New-RdsRoleAssignment -RoleDefinitionName "RDS Owner" -ApplicationId $svcPrincipal.AppId -TenantName $myTenantName
 
+$creds = New-Object System.Management.Automation.PSCredential($svcPrincipal.AppId, (ConvertTo-SecureString $svcPrincipalCreds.Value -AsPlainText -Force))
+Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com" -Credential $creds -ServicePrincipal -AadTenantId $aadContext.TenantId.Guid
 
+$svcPrincipalCreds.Value
+#password = +lE0xW/PQdhuwhZnjAhhUeDT5AQhyMbAp+EBxo67ixM=
 
+$aadContext.TenantId.Guid
+#tenant ID = fff86db3-851d-431d-a97a-a4434afc4a06
+
+$svcPrincipal.AppId
+#application ID = 8a303c08-633f-4d59-a13e-25fe172f7e1c
 
 
 
